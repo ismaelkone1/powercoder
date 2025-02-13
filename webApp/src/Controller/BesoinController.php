92499417a83e4ce2e1f17d09566b057afd7b14a4
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-02-13 13:56:11 +0100

    ajout du formulaire de création de besoin

--- webApp/src/Controller/BesoinController.php
+++ webApp/src/Controller/BesoinController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Besoin;
+use App\Form\BesoinType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 final class BesoinController extends AbstractController
 {
@@ -14,6 +18,27 @@ final class BesoinController extends AbstractController
 
         return $this->render('besoin/liste_besoin.html.twig', [
             'controller_name' => 'BesoinController',
+        ]);
+    }
+
+    #[Route('/besoin/create', name: 'create_besoin', methods: ['GET'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $besoin = new Besoin();
+        $form = $this->createForm(BesoinType::class, $besoin);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            dd($form->getData());
+            // $entityManager->persist($besoin);
+            // $entityManager->flush();
+
+            return $this->redirectToRoute('create_besoin');
+        }
+
+        return $this->render('besoin/create_besoin.html.twig', [
+            'controller_name' => 'BesoinController',
+            'form' => $form,
         ]);
     }
 }
