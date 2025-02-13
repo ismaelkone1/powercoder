70bd801059cc3f0c3e25139cd8e0be538e3ef839
niram <marin.bourdon2@etu.univ-lorraine.fr>
2025-02-13 11:54:19 +0100

    rajout score Affectation

--- optiApp/src/Affectation.java
+++ optiApp/src/Affectation.java
@@ -1,9 +1,15 @@
 class Affectation {
     private Salarie salarie;
     private Besoin besoin;
+    private double score;
 
-    public Affectation(Salarie salarie, Besoin besoin, int score) {
+    public Affectation(Salarie salarie, Besoin besoin, double score) {
         this.salarie = salarie;
         this.besoin = besoin;
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
     }
 }
