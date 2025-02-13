e77da021ea5681f25f34c34ec7524f7f3cc17b77
niram <marin.bourdon2@etu.univ-lorraine.fr>
2025-02-13 16:29:29 +0100

    maj out

--- optiApp/src/test/algo/MainEvaluation.java
+++ optiApp/src/test/algo/MainEvaluation.java
@@ -3,14 +3,18 @@ package test.algo;
 import test.Affectation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MainEvaluation {
 
     public static void main(String[] args) {
+        Map<Affectation, >
         // On crée n listes d'affectations
         for(int i = 0; i < 10; i++) {
-            List<Affectation> affectations = new ArrayList<>();
-            System.out.println("Hello world!");
+            // Tant que la nouvelle liste d'affectation est déjà existante
+
+                // On crée une liste d'affectation
+                List<Affectation> affectations = new ArrayList<>();
 
         }
 
