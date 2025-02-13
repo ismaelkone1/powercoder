7afa5a7f3db7887e0badff80f8426d2269d89674
niram <marin.bourdon2@etu.univ-lorraine.fr>
2025-02-13 11:49:22 +0100

    retrait score Affectation

--- optiApp/src/Affectation.java
+++ optiApp/src/Affectation.java
@@ -1,15 +1,9 @@
 class Affectation {
     private Salarie salarie;
     private Besoin besoin;
-    private int score;
 
     public Affectation(Salarie salarie, Besoin besoin, int score) {
         this.salarie = salarie;
         this.besoin = besoin;
-        this.score = score;
-    }
-
-    public int getScore() {
-        return score;
     }
 }
