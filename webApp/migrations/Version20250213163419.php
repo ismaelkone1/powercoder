95237cce2fc7ef9cfd4107a2f42dced3df2e3e7f
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2025-02-13 17:54:03 +0100

    Fichiers migrations

--- webApp/migrations/Version20250213163419.php
+++ webApp/migrations/Version20250213163419.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250213160641 extends AbstractMigration
+final class Version20250213163419 extends AbstractMigration
 {
     public function getDescription(): string
     {
