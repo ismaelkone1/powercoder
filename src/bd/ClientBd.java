a187ca4cf28984b5931fe4d0b0403f5e91b29a77
niram <marin.bourdon2@etu.univ-lorraine.fr>
2025-02-13 10:27:49 +0100

    ajout classe client pour interroger la bd (get/post)

---
+++ src/bd/ClientBd.java
@@ -0,0 +1,30 @@
+package bd;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ClientBd {
+
+    // methode d'exemple
+    public void getAllClients() {
+        String query = "SELECT id, nom, email FROM clients";
+
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(query);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nom = rs.getString("nom");
+                String email = rs.getString("email");
+
+                System.out.println("ID: " + id + ", Nom: " + nom + ", Email: " + email);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
